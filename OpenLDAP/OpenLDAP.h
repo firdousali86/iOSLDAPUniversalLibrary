56166aa5c8f288624ce4104244ef87a3a331987e
Firdous <firdous.ali86@gmail.com>
2016-02-29 11:46:32 +0500

    Initial Commit

---
+++ OpenLDAP/OpenLDAP.h
@@ -0,0 +1,13 @@
+//
+//  OpenLDAP.h
+//  OpenLDAP
+//
+//  Created by Firdous on 29/02/2016.
+//  Copyright © 2016 TenPearls. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenLDAP : NSObject
+
+@end
---
+++ OpenLDAP/OpenLDAP.m
@@ -0,0 +1,13 @@
+//
+//  OpenLDAP.m
+//  OpenLDAP
+//
+//  Created by Firdous on 29/02/2016.
+//  Copyright © 2016 TenPearls. All rights reserved.
+//
+
+#import "OpenLDAP.h"
+
+@implementation OpenLDAP
+
+@end
