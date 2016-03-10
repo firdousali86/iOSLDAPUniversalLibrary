df7b7f856d0069ad7acdb26b2ca6d57d8e4b3d8e
Firdous <firdous.ali86@gmail.com>
2016-03-10 15:08:01 +0500

    build folder

---
+++ build/OpenLDAP.build/Debug-iphoneos/OpenLDAP.build/Objects-normal/arm64/OpenLDAP.d
@@ -0,0 +1,7 @@
+dependencies: /Users/firdous/Desktop/OpenLDAP/OpenLDAP/OpenLDAP.m \
+  /Users/firdous/Desktop/OpenLDAP/OpenLDAP/OpenLDAP.h \
+  /Users/firdous/Desktop/OpenLDAP/OpenLDAP/ExternalLibraries/iOSPorts/include/ldap.h \
+  /Users/firdous/Desktop/OpenLDAP/OpenLDAP/ExternalLibraries/iOSPorts/include/lber.h \
+  /Users/firdous/Desktop/OpenLDAP/OpenLDAP/ExternalLibraries/iOSPorts/include/lber_types.h \
+  /Users/firdous/Desktop/OpenLDAP/OpenLDAP/ExternalLibraries/iOSPorts/include/ldap_cdefs.h \
+  /Users/firdous/Desktop/OpenLDAP/OpenLDAP/ExternalLibraries/iOSPorts/include/ldap_features.h
